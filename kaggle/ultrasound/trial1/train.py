f001d87ed78dfa4602dd960cc4022a7ce629fbbe
Eunmin <eunmin_lee@hotmail.com>
2016-07-17 17:47:30 +0900

    idk

--- kaggle/ultrasound/trial1/train.py
+++ kaggle/ultrasound/trial1/train.py
@@ -110,11 +110,13 @@ def conv2d(x, W, b, strides=1):
     x = tf.nn.bias_add(x, b)
     return tf.nn.relu(x)
 
-
 def maxpool2d(x, k=2):
     # MaxPool2D wrapper
     return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, k, k, 1],
                           padding='SAME')
+
+def conv2d_transpose(x, W, OS, strides=1):
+    return  tf.nn.conv2d_transpos(x, W, OS, strides=[1, strides, strides, 1], padding='SAME')
 
 
 # Create model
@@ -145,10 +147,18 @@ def conv_net(x, weights, biases, dropout):
     # Convolution Layer
     conv5 = conv2d(conv4, weights['wc5'], biases['bc5'])
 
-
+    # Deconolution Layer
+    conv6 = conv2d_transpose(conv5, weights['wc5'], outputshape['os6'])
+    # Deconolution Layer
+    conv7 = conv2d_transpose(conv6, weights['wc4'], outputshape['os7'])
+    # Deconolution Layer
+    conv8 = conv2d_transpose(conv7, weights['wc3'], outputshape['os8'])
+    # Deconolution Layer
+    conv9 = conv2d_transpose(conv8, weights['wc2'], outputshape['os9'])
+    
     # Fully connected layer
-    # Reshape conv2 output to fit fully connected layer input
-    fc1 = tf.reshape(conv2, [-1, weights['wd1'].get_shape().as_list()[0]])
+    # Reshape conv9 output to fit fully connected layer input
+    fc1 = tf.reshape(conv9, [-1, weights['wd1'].get_shape().as_list()[0]])
     fc1 = tf.add(tf.matmul(fc1, weights['wd1']), biases['bd1'])
     fc1 = tf.nn.relu(fc1)
     # Apply Dropout
@@ -177,7 +187,12 @@ weights = {
     # 1024 inputs, 10 outputs (class prediction)
     'out': tf.Variable(tf.random_normal([1024, n_classes]))
 }
-
+outputshape = {
+    'os6': [batch_size, 2, 2, 256],
+    'os7': [batch_size, 2, 2, 128],
+    'os8': [batch_size, 2, 2,  64],
+    'os9': [batch_size, 2, 2,  32]
+}
 biases = {
     'bc1': tf.Variable(tf.random_normal([32])),
     'bc2': tf.Variable(tf.random_normal([64])),

52bc6fb9fd7d75fbaf8d6e1df2798ac93130a232
Eunmin Lee <eunmin_lee@hotmail.com>
2016-07-23 13:48:22 -0500

    sequential file

---
+++ kaggle/ultrasound/trial1/train_sq.py
@@ -0,0 +1,131 @@
+from __future__ import print_function
+
+import cv2
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Input, Dense, Convolution2D, MaxPooling2D, Flatten
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from keras import backend as K
+
+from data import load_train_data, load_test_data
+
+img_rows = 64
+img_cols = 80
+
+smooth = 1.
+
+
+def dice_coef(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+
+def dice_coef_loss(y_true, y_pred):
+    return -dice_coef(y_true, y_pred)
+
+
+def get_unet():
+    inputs = Input()
+
+
+    return model
+
+
+def preprocess(imgs):
+    imgs_p = np.ndarray((imgs.shape[0], imgs.shape[1], img_rows, img_cols), dtype=np.uint8)
+    for i in range(imgs.shape[0]):
+        imgs_p[i, 0] = cv2.resize(imgs[i, 0], (img_cols, img_rows), interpolation=cv2.INTER_CUBIC)
+    return imgs_p
+
+
+print('-'*30)
+print('Loading and preprocessing train data...')
+print('-'*30)
+imgs_train, imgs_mask_train = load_train_data()
+
+imgs_train = preprocess(imgs_train)
+imgs_mask_train = preprocess(imgs_mask_train)
+
+imgs_train = imgs_train.astype('float32')
+mean = np.mean(imgs_train)  # mean for data centering
+std = np.std(imgs_train)  # std for data normalization
+
+imgs_train -= mean
+imgs_train /= std
+
+imgs_mask_train = imgs_mask_train.astype('float32')
+imgs_mask_train /= 255.  # scale masks to [0, 1]
+
+print('-'*30)
+print('Creating and compiling model...')
+print('-'*30)
+model = Sequential()
+
+model.add(Convolution2D(32, 3, 3, border_mode='same', input_shape=(1, img_rows, img_cols)))
+model.add(Activation('relu'))
+model.add(Convolution2D(32, 3, 3))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Convolution2D(64, 3, 3))
+model.add(Activation('relu'))
+model.add(Convolution2D(64, 3, 3))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Convolution2D(128, 3, 3))
+model.add(Activation('relu'))
+model.add(Convolution2D(128, 3, 3))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Convolution2D(256, 3, 3))
+model.add(Activation('relu'))
+model.add(Convolution2D(256, 3, 3))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Convolution2D(512, 3, 3))
+model.add(Activation('relu'))
+model.add(Convolution2D(512, 3, 3))
+model.add(Activation('relu'))
+
+model.add(Flatten())
+
+model.add(Dense(512))
+
+
+model.compile(optimizer=Adam(lr=1e-5), loss=dice_coef_loss, metrics=[dice_coef])
+
+
+model_checkpoint = ModelCheckpoint('unet.hdf5', monitor='loss', save_best_only=True)
+
+print('-'*30)
+print('Fitting model...')
+print('-'*30)
+model.fit(imgs_train, imgs_mask_train, batch_size=32, nb_epoch=20, verbose=1, shuffle=True,
+          callbacks=[model_checkpoint])
+
+print('-'*30)
+print('Loading and preprocessing test data...')
+print('-'*30)
+imgs_test, imgs_id_test = load_test_data()
+imgs_test = preprocess(imgs_test)
+
+imgs_test = imgs_test.astype('float32')
+imgs_test -= mean
+imgs_test /= std
+
+print('-'*30)
+print('Loading saved weights...')
+print('-'*30)
+model.load_weights('unet.hdf5')
+
+print('-'*30)
+print('Predicting masks on test data...')
+print('-'*30)
+imgs_mask_test = model.predict(imgs_test, verbose=1)
+np.save('imgs_mask_test.npy', imgs_mask_test)

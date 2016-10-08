20137339f6aa00ca24fb40fdf89db4d334c17a65
Eddie Lee <Eddie Lee>
2016-10-08 14:27:17 +0900

    edited

--- kaggle/utils/csvtools.py
+++ kaggle/utils/csvtools.py
@@ -1,5 +1,9 @@
 import csv
-with open('FILENAME.csv', 'rb') as csvfile:
+with open('test_date.csv', 'rb') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    cnt = 0
     for row in spamreader:
         print ', '.join(row)
+        cnt = cnt + 1
+        if cnt == 2:
+            break

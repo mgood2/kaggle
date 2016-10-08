79aeede5e0b334017626c4515c2732d29e561411
Eddie Lee <Eddie Lee>
2016-10-08 14:25:17 +0900

    pandas tutorials

---
+++ kaggle/utils/tutorial/tutorial.py
@@ -0,0 +1,11 @@
+from pandas import DataFrame, read_csv
+
+import matplotlib.pyplot as pyplot
+import pandas as pd
+import sys
+import matplotlib
+
+
+print('Python version ' + sys.version)
+print('Pandas version ' + pd.__version__)
+print('Matplotlib version ' + matplotlib.__version__)
